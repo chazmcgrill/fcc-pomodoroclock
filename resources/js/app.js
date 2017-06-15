3d1150ee5e9ee85e515da63f5c6c9b77d4e6a5cc
chazmcgrill <charlietaylordev@gmail.com>
2017-06-15 18:35:05 +0100

    Added Pause Functionality
    Pause functionality added using a flag variable.  Cleaned up variable
    names.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,14 @@
-var minCount = 2,
-    secCount = 60,
+
+// User Story: I can start a 25 minute pomodoro, and the timer will go off once 25 minutes has elapsed.
+// User Story: I can reset the clock for my next pomodoro.
+// 1 second digit needs a zero in front
+
+var seconds = 60,
     count = 0,
     minBreak = 2,
-    status = 'break';
-
-// use the same timer switch between work and break
-// timer(min, sec, count);
+    minSesh = 2,
+    status = 'break',
+    pauseFlag = false;
 
 // timer function
 function timer(min, sec, counter) {
@@ -23,47 +26,61 @@ function timer(min, sec, counter) {
       chooser();
     }
 
-    console.log(counter + ' second');
-    sec--;
-    $('.seconds').text(sec);
-    counter--;
+    if (!pauseFlag) {
+      console.log(counter + ' second');
+      sec--;
+      $('.seconds').text(sec);
+      counter--;
+    }
 
   }, 1000);
 
 }
 
+// function that switches between break and session
 function chooser() {
-
   if(status === 'session') {
     status = 'break';
-    count = minBreak * secCount;
-    timer(minBreak, secCount, count);
+    count = minBreak * seconds;
+    timer(minBreak, seconds, count);
   } else {
     status = 'session';
-    count = minCount * secCount;
-    timer(minCount, secCount, count);
+    count = minSesh * seconds;
+    timer(minSesh, seconds, count);
   }
-
 }
 
+// start / pause button
 $('.btn-start').click(function() {
-  count = minCount * secCount;
-  chooser();
+  var thus = $(this).text();
+  if (thus === 'Pause') {
+    pauseFlag = true;
+    $(this).text('Back to work');
+  } else if (thus === 'Back to work') {
+    pauseFlag = false;
+    $(this).text('Pause');
+  } else {
+    count = minSesh * seconds;
+    $('.btn-elements').prop('disabled', true)
+    $(this).text('Pause');
+    chooser();
+  }
 });
 
-
+// session adjustment buttons
 $('.session-plus').click(function() {
-  minCount++;
-  $('.minutes').text(minCount);
-  $('.session-num').text(minCount);
+  minSesh++;
+  $('.minutes').text(minSesh);
+  $('.session-num').text(minSesh);
 });
 
 $('.session-minus').click(function() {
-  minCount--;
-  $('.minutes').text(minCount);
-  $('.session-num').text(minCount);
+  minSesh--;
+  $('.minutes').text(minSesh);
+  $('.session-num').text(minSesh);
 });
 
+// break adjustment buttons
 $('.break-plus').click(function() {
   minBreak++;
   $('.break-num').text(minBreak);
